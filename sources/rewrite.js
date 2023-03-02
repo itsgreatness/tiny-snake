c406ad2d23a73fbd029e934c0f6b5e61d8732cb7
itsgreatness <dzhao650@gmail.com>
2023-03-01 18:45:16 -0600

    but maybe its more organised?

--- sources/rewrite.js
+++ sources/rewrite.js
@@ -1,14 +1,14 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
 javascript: (function () {
     "use strict";
+    var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+        if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+            if (ar || !(i in from)) {
+                if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+                ar[i] = from[i];
+            }
+        }
+        return to.concat(ar || Array.prototype.slice.call(from));
+    };
     var _a;
     if (typeof globalThis.globalThis.active !== "boolean") {
         var _id;
